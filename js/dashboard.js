7ada28972eeac51e220663a6d40621ad9d33906b
Sanad Thapa <sanadtm@gmail.com>
2021-11-27 15:23:27 -0800

    added 7 day chart of 1 currency

--- js/dashboard.js
+++ js/dashboard.js
@@ -95,12 +95,8 @@ function formatDate(date) {
 	return [year, month, day].join("-");
 }
 const top_5_curr = () => {
-	//let app = document.querySelector(".dates");
 	let yes_price_data = "";
-	let price_data = "";
 	let curr_name = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json";
-	let curr_price_url =
-		"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
 
 	fetch(curr_name) // Getting Currency Name
 		.then((response) => response.json())
@@ -110,7 +106,6 @@ const top_5_curr = () => {
 	let date_label = [];
 	seven = [];
 	date_label.push(formatDate(new Date()));
-	for (let i = 1; i < 8; ++i) {}
 	const today = new Date();
 	const yesterday = new Date(today);
 	const day_before = new Date(yesterday);
@@ -121,10 +116,10 @@ const top_5_curr = () => {
 
 	yesterday.setDate(yesterday.getDate() - 1);
 	day_before.setDate(day_before.getDate() - 2);
-	day_before1.setDate(day_before.getDate() - 3);
-	day_before2.setDate(day_before.getDate() - 4);
-	day_before3.setDate(day_before.getDate() - 5);
-	day_before4.setDate(day_before.getDate() - 6);
+	day_before1.setDate(day_before1.getDate() - 3);
+	day_before2.setDate(day_before2.getDate() - 4);
+	day_before3.setDate(day_before3.getDate() - 5);
+	day_before4.setDate(day_before4.getDate() - 6);
 
 	today.toDateString();
 	yesterday.toDateString();
@@ -141,7 +136,18 @@ const top_5_curr = () => {
 	let day_before_yes2 = formatDate(day_before2);
 	let day_before_yes3 = formatDate(day_before3);
 	let day_before_yes4 = formatDate(day_before4);
+	console.log(
+		todays_date,
+		yesterday_date,
+		day_before_yes,
+		day_before_yes1,
+		day_before_yes2,
+		day_before_yes3,
+		day_before_yes4
+	);
+	let today_price_url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${todays_date}/currencies/usd.json`;
 
+	let yes_price_url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${yesterday_date}/currencies/usd.json`;
 	let day_before_url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${day_before_yes}/currencies/usd.json`;
 	let day_before_url1 = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${day_before_yes1}/currencies/usd.json`;
 	let day_before_url2 = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${day_before_yes2}/currencies/usd.json`;
@@ -161,10 +167,10 @@ const top_5_curr = () => {
 	datetoDOM("A Week Ago Date  :: " + yesterday_date);
 	console.log("TODAYS DATE " + todays_date);
 	console.log("Yesterdays DATE " + yesterday_date);
-	let yes_price_url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${yesterday_date}/currencies/usd.json`;
+
 	// Todays Price
-	async function get_price(curr_price) {
-		let response = await fetch(curr_price);
+	async function get_price(curr) {
+		let response = await fetch(curr);
 		let data = await response.json();
 		return data;
 	}
@@ -172,19 +178,32 @@ const top_5_curr = () => {
 	async function yesterdays_price(yes_price) {
 		let response = await fetch(yes_price);
 		let data = await response.json();
+		//console.log(data);
 		return data;
 	}
-	async function get_data() {
-		price_data = await get_price(curr_price_url);
-		yes_price_data = await yesterdays_price(yes_price_url);
-		//console.log(price_data);
-		//console.log(yes_price_data);
-		let data1 = price_data.usd;
-		let data2 = yes_price_data.usd;
-		let difference = [];
-		//console.log(data1);
-		//console.log(data2);
 
+	async function get_data() {
+		//today_price_data = await get_price(today_price_url);
+		get_names = await get_price(curr_name);
+		console.log(get_names);
+		todays_price = await get_price(yes_price_url);
+		yes_price_data = await yesterdays_price(yes_price_url);
+		yes_price_data1 = await yesterdays_price(day_before_url);
+		yes_price_data2 = await yesterdays_price(day_before_url1);
+		yes_price_data3 = await yesterdays_price(day_before_url2);
+		yes_price_data4 = await yesterdays_price(day_before_url3);
+		yes_price_data5 = await yesterdays_price(day_before_url4);
+		let data1 = yes_price_data.usd;
+		let data2 = yes_price_data1.usd;
+		let data3 = yes_price_data2.usd;
+		let data4 = yes_price_data3.usd;
+		let data5 = yes_price_data4.usd;
+		let data6 = yes_price_data5.usd;
+
+		console.log(data1);
+		console.log(data2);
+		console.log(data3);
+		console.log(typeof data4);
 		// Finding the TOP PRICE MOVERS!!
 		let curr = [];
 		let day1 = [];
@@ -194,45 +213,27 @@ const top_5_curr = () => {
 			day1.push(Number(`${data1[each]}`));
 		}
 		let diff = "3.14";
-		//console.log(Number(diff) +1);
-		//console.log(typeof diff);
 		for (const each in data2) {
 			day2.push(Number(`${data2[each]}`));
 		}
 
-		console.log(typeof parseFloat(day1[0]));
 		for (let i = 0; i < day1.length; ++i) {
 			let increase = 0;
 			let decrease = 0;
-			//console.log(day1[i]);
-			//console.log(day2[i]);
-			//console.log(typeof day2[i]);
 			diff = (day1[i] - day2[i]).toPrecision(6);
 			if (diff > 0) {
 				increase = ((diff / day2[i]) * 100).toPrecision(9);
-				difference.push(increase);
 			} else {
 				decrease = ((diff / day1[i]) * 100).toPrecision(9);
 				-Math.abs(decrease);
-				difference.push(decrease);
 			}
-			//	console.log(" percentage " + increase);
-			//	console.log(" percentage " + decrease);
-			//console.log(i);
-			//difference.push(diff);
 		}
-		//console.log(difference);
-
-		//Sorting the Array Object to find Top and Bottom Gainers
 		var sorted_price = {};
-		for (var i = 0; i < curr.length; i++) {
-			sorted_price[curr[i]] = difference[i];
-		}
 		//console.log(sorted_price);
 		const sortable = Object.entries(sorted_price)
 			.sort(([, a], [, b]) => a - b)
 			.reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
-		console.log(sortable);
+		//console.log(sortable);
 		let Gainer_label = [];
 		let Gainer_data = [];
 		let Looser_label = [];
@@ -242,24 +243,16 @@ const top_5_curr = () => {
 				value = sortable[key];
 			Looser_label.push(key);
 			Looser_data.push(value);
-
-			//console.log(key, value);
-			//ToptoDOM(key.toUpperCase(), value + " %");
 		}
-		console.log(Gainer_label);
-		console.log(Gainer_data);
 		const sortable1 = Object.entries(sorted_price)
 			.sort(([, a], [, b]) => b - a)
 			.reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
-		console.log(sortable1);
 		for (let keys = Object.keys(sortable1), i = 0, end = 8; i < end; ++i) {
 			let key = keys[i],
 				value = sortable1[key];
-			Gainer_label.push(key.toLocaleUpperCase());
+			Gainer_label.push(key);
 			Gainer_data.push(value);
 		}
-
-		console.log(day_before_yes, yesterday_date, todays_date);
 		let bar_chart = document.getElementById("lineChart");
 		let myChart = new Chart(bar_chart, {
 			type: "line",
@@ -268,125 +261,18 @@ const top_5_curr = () => {
 			responsiveAnimationDuration: 2,
 
 			data: {
-				labels: [, , , todays_date, day_before_yes, yesterday_date, todays_date],
+				labels: [
+					yesterday_date,
+					day_before_yes,
+					day_before_yes1,
+					day_before_yes2,
+					day_before_yes3,
+					day_before_yes4,
+				],
 				datasets: [
 					{
-						label: Gainer_label[0],
-						data: Gainer_data[0],
-						borderColor: [
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-					{
-						label: Gainer_label[1],
-						data: Gainer_data[1],
-						borderColor: [
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-					{
-						label: Gainer_label[2],
-						data: Gainer_data[2],
-						borderColor: [
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-					{
-						label: Gainer_label[3],
-						data: Gainer_data[3],
-						borderColor: [
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-					{
-						label: Gainer_label[4],
-						data: Gainer_data[4],
-						borderColor: [
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-					{
-						label: Gainer_label[5],
-						data: Gainer_data[5],
-						borderColor: [
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-							"rgba(255,99,132,1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-					{
-						label: Gainer_label[6],
-						data: Gainer_data[6],
+						label: get_names.ada,
+						data: [data1.ada, data2.ada, data3.ada, data4.ada, data5.ada, data6.ada],
 						borderColor: [
 							"rgba(255,99,132,1)",
 							"rgba(54, 162, 235, 1)",
